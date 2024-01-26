8ea502025cc1b8f63657a6e47961c350678e6015
Inu Park <dlsdn14@naver.com>
2024-01-26 19:15:30 +0900

    드래그앤드롭 애니메이션

--- App.js
+++ App.js
@@ -1,21 +1,165 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import styled from 'styled-components/native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Animated, Easing, PanResponder, View } from 'react-native';
+import icons from './icons';
+
+const BLACK_COLOR = '#1e272e';
+const GREY = '#485460';
+const GREEN = '#2ecc71';
+const RED = '#e74c3c';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${BLACK_COLOR};
+`;
+const Edge = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const WordContainer = styled(Animated.createAnimatedComponent(View))`
+  height: 100px;
+  width: 100px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${GREY};
+  border-radius: 100px;
+`;
+
+const Word = styled.Text`
+  font-size: 30px;
+  font-weight: 500;
+  color: ${(props) => props.color};
+`;
+const Center = styled.View`
+  flex: 3;
+  align-items: center;
+  justify-content: center;
+`;
+const IconCard = styled(Animated.createAnimatedComponent(View))`
+  background-color: white;
+  padding: 15px 20px;
+  border-radius: 10px;
+  z-index: 10;
+`;
 
 export default function App() {
+  // Values
+  const opacity = useRef(new Animated.Value(1)).current;
+  const scale = useRef(new Animated.Value(1)).current;
+  const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+  const scaleOne = position.y.interpolate({
+    inputRange: [-300, -80],
+    outputRange: [2, 1],
+    extrapolate: 'clamp',
+  });
+  const scaleTwo = position.y.interpolate({
+    inputRange: [80, 300],
+    outputRange: [1, 2],
+    extrapolate: 'clamp',
+  });
+
+  // Animations
+  const onPressIn = Animated.spring(scale, {
+    toValue: 0.9,
+    useNativeDriver: true,
+  });
+  const onPressOut = Animated.spring(scale, {
+    toValue: 1,
+    useNativeDriver: true,
+  });
+  const goHome = Animated.spring(position, {
+    toValue: {
+      x: 0,
+      y: 0,
+    },
+    useNativeDriver: true,
+  });
+  const onDropScale = Animated.timing(scale, {
+    toValue: 0,
+    useNativeDriver: true,
+    duration: 50,
+    easing: Easing.linear,
+  });
+  const onDropOpacity = Animated.timing(opacity, {
+    toValue: 0,
+    duration: 50,
+    easing: Easing.linear,
+    useNativeDriver: true,
+  });
+
+  // Pan Responders
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        onPressIn.start();
+      },
+      onPanResponderMove: (_, { dx, dy }) => {
+        position.setValue({
+          x: dx,
+          y: dy,
+        });
+      },
+      onPanResponderRelease: (_, { dy }) => {
+        if (dy < -230 || dy > 230) {
+          Animated.sequence([
+            Animated.parallel([onDropScale, onDropOpacity]),
+            Animated.timing(position, {
+              toValue: 0,
+              duration: 50,
+              easing: Easing.linear,
+              useNativeDriver: true,
+            }),
+          ]).start(nextIcon);
+        } else {
+          Animated.parallel([onPressOut, goHome]).start();
+        }
+      },
+    })
+  ).current;
+
+  // State
+  const [index, setIndex] = useState(0);
+  const nextIcon = () => {
+    Animated.parallel([
+      Animated.spring(opacity, {
+        toValue: 1,
+        useNativeDriver: true,
+      }),
+      Animated.spring(scale, {
+        toValue: 1,
+        useNativeDriver: true,
+      }),
+    ]).start();
+
+    setIndex((prev) => prev + 1);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Edge>
+        <WordContainer style={{ transform: [{ scale: scaleOne }] }}>
+          <Word color={GREEN}>알아</Word>
+        </WordContainer>
+      </Edge>
+
+      <Center>
+        <IconCard
+          {...panResponder.panHandlers}
+          style={{
+            opacity,
+            transform: [...position.getTranslateTransform(), { scale }],
+          }}>
+          <Ionicons name={icons[index]} color={GREY} size={76} />
+        </IconCard>
+      </Center>
+
+      <Edge>
+        <WordContainer style={{ transform: [{ scale: scaleTwo }] }}>
+          <Word color={RED}>몰라</Word>
+        </WordContainer>
+      </Edge>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
